8123dcc59ae8809a3a742f54faff47fd5c97d62c
juankmanca <juankmanca@gmail.com>
2024-04-03 15:08:19 -0500

    Game Controller

---
+++ RPS_Game/src/RPS_Game.API/Controllers/GameController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RPS_Game.API.Models;
+
+namespace RPS_Game.API.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]/[action]")]
+    public class GameController : ControllerBase
+    {
+        [HttpGet]
+        [ActionName("hi")]
+        public async Task<IActionResult> Hi()
+        {
+
+            return Ok();
+        }
+
+
+        [HttpPost]
+        [ActionName("registerPlayers")]
+
+        public async Task<IActionResult> RegisterPlayers([FromBody] RegisterPlayersQuery request)
+        {
+            Console.Write(request.player1);
+
+            return Ok();
+        }
+    }
+}
---
+++ RPS_Game/src/RPS_Game.API/Models/RegisterPlayersQuery.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RPS_Game.API.Models
+{
+    public sealed class RegisterPlayersQuery
+    {
+        [Required]
+        [MaxLength(50)]
+        public string player1 {  get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(50)]
+        public string player2 { get; set; } = string.Empty;
+    }
+}
